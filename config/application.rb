00a46c7f553c8ca58d1d08be2df54f61d698806f
Danilo Alfaro <alexander.danilo.alfaro@gmail.com>
2020-05-04 10:21:30 -0600

    No check relationships

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module ABetterApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    # config.active_record.belongs_to_required_by_default = false
+    config.active_record.belongs_to_required_by_default = false
 
     # Settings in config/environments/*
     # take precedence over those specified here.
