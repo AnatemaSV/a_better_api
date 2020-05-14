9c310eb24a5c2d40cc6b19faacfe93832a2dd4e0
Danilo Alfaro <alexander.danilo.alfaro@gmail.com>
2020-05-14 15:56:39 -0600

    Fix error in params

--- app/controllers/api/v1/inforequests_controller.rb
+++ app/controllers/api/v1/inforequests_controller.rb
@@ -40,30 +40,30 @@ module Api
         pp = params[:per_page] || 5
 
         @inforequests = Inforequest.all
-        if params[:query].present? && params[:query].blank?
+        if params[:query].present? && !params[:query].blank?
           @inforequests = @inforequest.search_by_term(params[:query])
         end
 
-        if params[:'date-begin'].present? && params[:'date-begin'].blank?
+        if params[:'date-begin'].present? && !params[:'date-begin'].blank?
           @inforequests = @inforequests.dbegin(params[:'date-begin'])
         end
 
-        if params[:'date-end'].present? && params[:'date-end'].blank?
+        if params[:'date-end'].present? && !params[:'date-end'].blank?
           @inforequests = @inforequests.dend(params[:'date-end'])
         end
 
-        if params[:result].present? && params[:result].blank?
+        if params[:result].present? && !params[:result].blank?
           index = params[:result].to_i
           result = Result.find_by_name(Inforequest::RESULTS[index]) ? Result.find_by_name(Inforequest::RESULTS[index]) : Result.first
           @inforequests = @inforequests.rtext(result.id)
         end
 
-        if params[:status].present? && params[:status].blank?
+        if params[:status].present? && !params[:status].blank?
           status = params[:status].to_i.positive? ? Status.find_by(name: 'En trámite').id : Status.find_by(name: 'Cerrada').id
           @inforequests = @inforequests.stext(status)
         end
 
-        if params[:institution].present? && params[:institution].blank?
+        if params[:institution].present? && !params[:institution].blank?
           institution = Institution.exists?(id: params[:institution].to_i) ? Institution.find(params[:institution].to_i).id : Institution.first.id
           @inforequests = @inforequests.by_institution(institution)
         end
