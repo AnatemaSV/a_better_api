d7bb4991bdf14dad587fd13ad72a1d4c5db7fc41
Danilo Alfaro <alexander.danilo.alfaro@gmail.com>
2020-05-18 16:09:43 -0600

    ¬_¬

--- config/application.rb
+++ config/application.rb
@@ -38,10 +38,15 @@ module ABetterApi
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Request-Method' => %w[GET POST OPTIONS].join(',')
+    }
+
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: %i[get post options]
+        resource '*', headers: :any, methods: %i[GET POST OPTIONS]
       end
     end
   end
